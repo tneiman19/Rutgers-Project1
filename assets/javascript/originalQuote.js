074128da07c25b7c60f427139cad63e0d6122505
Tzvi Neiman <tneiman19@gmail.com>
2023-02-06 20:02:10 -0500

    created fetch statement to get a quote and author from the api.  Created a function to take the result and set variables to the result values.   Created a function to place the text of the author and quoye in the html

---
+++ assets/javascript/originalQuote.js
@@ -0,0 +1,35 @@
+var category = "happiness";
+
+fetch("https://api.api-ninjas.com/v1/quotes?category=" + category, {
+  method: "GET",
+  headers: {
+    "X-Api-Key": "5x71s2TVATKI4DvhobMvrw==t5uBHyqBLjdXumUv",
+    "Content-Type": "application/json",
+  },
+})
+  .then(function (response) {
+    //console.log(response)
+    return response.json();
+  })
+  .then(function (result) {
+    getQuoteInfo(result)
+    displayQuoteInfo();
+    // console.log(result);
+    // console.log(result[0].author);
+    // console.log(result[0].quote);
+  })
+  .catch(function (error) {
+    console.error("Error: ", error);
+  });
+
+  //This function takes in a fetch result and assigns values to variables
+  function getQuoteInfo(result){
+    author = result[0].author
+    originalQuote = result[0].quote
+    console.log(author, originalQuote)
+  }
+
+  function displayQuoteInfo(){
+   authorEl.textContent = author
+   quoteEl.textContent = originalQuote
+  }
