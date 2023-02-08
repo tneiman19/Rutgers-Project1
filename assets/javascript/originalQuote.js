d7c730b1ef6573920a266f44471418308af230f0
Tzvi Neiman <tneiman19@gmail.com>
2023-02-07 19:46:09 -0500

    Added function to run the first fetch when the button is clicked.

--- assets/javascript/originalQuote.js
+++ assets/javascript/originalQuote.js
@@ -1,31 +1,34 @@
-// Call this function on page load
-populateCategoryDropdown(categoryList)
+// Call this function on page load to populate the dropdown options
+populateCategoryDropdown(categoryList);
 
-var category = "happiness";
+getQuoteButton.addEventListener("click", fetchOriginalQuote);
 
-getQuoteButton.addEventListener("click", () => console.log(categoryList.value));
+function fetchOriginalQuote() {
+  category = categoryDropdown.value;
+  console.log(category);
 
-fetch("https://api.api-ninjas.com/v1/quotes?category=" + category, {
-  method: "GET",
-  headers: {
-    "X-Api-Key": "5x71s2TVATKI4DvhobMvrw==t5uBHyqBLjdXumUv",
-    "Content-Type": "application/json",
-  },
-})
-  .then(function (response) {
-    //console.log(response)
-    return response.json();
+  fetch("https://api.api-ninjas.com/v1/quotes?category=" + category, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": "5x71s2TVATKI4DvhobMvrw==t5uBHyqBLjdXumUv",
+      "Content-Type": "application/json",
+    },
   })
-  .then(function (result) {
-    getQuoteInfo(result)
-    displayQuoteInfo();
-    // console.log(result);
-    // console.log(result[0].author);
-    // console.log(result[0].quote);
-  })
-  .catch(function (error) {
-    console.error("Error: ", error);
-  });
+    .then(function (response) {
+      //console.log(response)
+      return response.json();
+    })
+    .then(function (result) {
+      getQuoteInfo(result);
+      displayQuoteInfo();
+      // console.log(result);
+      // console.log(result[0].author);
+      // console.log(result[0].quote);
+    })
+    .catch(function (error) {
+      console.error("Error: ", error);
+    });
+}
 
 //This function takes in a fetch result and assigns values to variables
 function getQuoteInfo(result) {
@@ -34,18 +37,18 @@ function getQuoteInfo(result) {
   console.log(author, originalQuote);
 }
 
+//This function sets the author and quote on the DOM
 function displayQuoteInfo() {
   authorEl.textContent = "Author: " + author;
   quoteEl.textContent = "Quote: " + originalQuote;
 }
 
-// This function generates the options for the category dropdown  
-function populateCategoryDropdown(array){
+// This function generates the options for the category dropdown
+function populateCategoryDropdown(array) {
   for (let i = 0; i < array.length; i++) {
-    var option = document.createElement('option')
-    option.setAttribute('value', array[i])
-    option.textContent = array[i]
-    categoryDropdown.appendChild(option)
-    
+    var option = document.createElement("option");
+    option.setAttribute("value", array[i]);
+    option.textContent = array[i];
+    categoryDropdown.appendChild(option);
   }
 }
