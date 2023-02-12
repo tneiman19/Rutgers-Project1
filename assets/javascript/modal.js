540452b439536bf5ff783ecb7aa6e410b29ce54f
Tzvi Neiman <tneiman19@gmail.com>
2023-02-12 16:46:52 -0500

    Fixed bug where buttons would lnot open modal after new button was added.  Added to the README file as well.

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -8,30 +8,35 @@ var btns = document.getElementsByClassName("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // Loop through all the buttons and attach click event to each button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].onclick = function(e) {
-    modal.style.display = "block";
-    var modalIndex = e.target.getAttribute('data-index')
-    var dataAuthor = e.target.getAttribute('data-author')
-    var dataQuote1 = e.target.getAttribute('data-quote1')
-    var dataQuote2 = e.target.getAttribute('data-quote2')
-    var dataLanguage = e.target.getAttribute('data-language')
+function btnEvent(){
 
-   document.getElementById('authorModalSpan').textContent = dataAuthor
-   document.getElementById('quoteModalSpan').textContent = dataQuote1
-   document.getElementById('translatedModalSpan').textContent = dataQuote2
-   document.getElementById('languageModalSpan').textContent = dataLanguage
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function (e) {
+      modal.style.display = "block";
+      var modalIndex = e.target.getAttribute("data-index");
+      var dataAuthor = e.target.getAttribute("data-author");
+      var dataQuote1 = e.target.getAttribute("data-quote1");
+      var dataQuote2 = e.target.getAttribute("data-quote2");
+      var dataLanguage = e.target.getAttribute("data-language");
+
+      document.getElementById("authorModalSpan").textContent = dataAuthor;
+      document.getElementById("quoteModalSpan").textContent = dataQuote1;
+      document.getElementById("translatedModalSpan").textContent = dataQuote2;
+      document.getElementById("languageModalSpan").textContent = dataLanguage;
+    };
   }
 }
 
+btnEvent()
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
